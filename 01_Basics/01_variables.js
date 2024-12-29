8e0673dd85df492b8073ed76afd7bb03c230121a
Mohit Prasoya <alaxsam277@gmail.com>
2024-12-29 10:51:21 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 123456
+let accountEmail="mohit@gmail.com"
+var accountPassword="789345"
+ accountCity= "Jaipur"
+
+ accountEmail="rahul@gmail.com"
+ accountPassword="212151"
+ accountCity="bangalore"
+
+ console.log(accountId)
+
+ console.table([accountEmail,accountId,accountPassword])
