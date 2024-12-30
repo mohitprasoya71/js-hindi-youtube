4d90539e530d2e009f0aabb1e142977646f8f46b
Mohit Prasoya <alaxsam277@gmail.com>
2024-12-30 07:02:02 +0000

    conversions

---
+++ 01_Basics/04_conversions.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2" >1);
+// console.log("02">1);
+
+console.log(null>0);  //Here there is one equality check and one is comparsion operator. So comparsion will convert null to 0
+console.log(null ==0);
+console.log(null>=0);
+
+console.log(undefined ==0)
+console.log(undefined >0)
+console.log(undefined <0)
+
+// ===
+
+console.log("2"===2);
